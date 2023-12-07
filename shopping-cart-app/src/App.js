cbf01fb82a2d3ac3db30eddc3b9d8325faec000c
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2023-12-07 13:27:09 +0300

    components are created

--- shopping-cart-app/src/App.js
+++ shopping-cart-app/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import "./styles.css";
+import { Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      
+      <h1>
+        Alışveriş Sepeti Yapımı
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+        React Dersleri
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
     </div>
   );
 }
-
-export default App;
