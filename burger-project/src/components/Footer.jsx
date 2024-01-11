e7e56dbc8ac32d55ad868d7fefdb4c5577c8bd5f
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2024-01-11 15:34:51 +0300

    footer is edited with its css

--- burger-project/src/components/Footer.jsx
+++ burger-project/src/components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "../styles/Footer.css"
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='socialMedia'>
+        <FaFacebookSquare/>
+        <FaInstagram/>
+        <FaTwitter/>
+      </div>
+      <p>Tüm Hakları Saklıdır | MFO</p>
+    </div>
   )
 }
 
