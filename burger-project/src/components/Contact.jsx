3cccac25cf74524662002fc6f95ec2aeab0cbebf
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2024-01-12 19:47:27 +0300

    Contact part is edited and finished

--- burger-project/src/components/Contact.jsx
+++ burger-project/src/components/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "../styles/Contact.css"
+import BannerImage from '../assets/banner.jpg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide' style={{backgroundImage:`url(${BannerImage})`}}>
+        
+      </div>
+      <div className='rightSide'>
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input type='text' placeholder='Lütfen adınızı soyadınızı giriniz...'/>
+          <label>Email</label>
+          <input type='email' placeholder='Lütfen mailinizi giriniz...'/>
+          <label>Mesajınız</label>
+          <textarea placeholder='Lütfen mesajınızı giriniz...' rows="6"></textarea>
+          <button>Gönder</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
