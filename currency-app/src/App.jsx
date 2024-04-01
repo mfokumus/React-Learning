7799cb5f564fa8c537e734bb73a0a80440b002dd
MeteFurkanOkumus <metefurkanokumus@gmail.com>
2024-04-01 15:36:04 +0300

    Css files are added

--- currency-app/src/App.jsx
+++ currency-app/src/App.jsx
@@ -1,12 +1,13 @@
+import "./App.css"
 import Currency from "./components/Currency";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Navbar />
       <Currency />
-    </>
+    </div>
   );
 }
 
